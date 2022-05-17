9d93e40eebc09418eb5ecab9fcd336544e0364b9
FrankLai <laihongjiang@foxmail.com>
2022-05-17 23:19:12 +0800

    制作footer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import {Chart1} from '../components/chart-1';
 import { Chart2 } from '../components/chart-2';
 
 export const Home = () => {
+  const year = new Date().getFullYear();
   return (
     <div className='home'>
       <header style={{backgroundImage:`url(${headerBg})`}}/>
@@ -19,6 +20,9 @@ export const Home = () => {
         <section className='bordered section4'></section>
         <section className='bordered section5'></section>
       </main>
+      <footer>
+        &copy; 拾伍 2022-{year}
+      </footer>
     </div>
   );
 };
