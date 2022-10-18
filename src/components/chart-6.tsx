ae8d74d9e828f871df6bbd00b41fc780ec8239c1
FrankLai <laihongjiang@foxmail.com>
2022-10-19 03:07:57 +0800

    优化地图组件

--- src/components/chart-6.tsx
+++ src/components/chart-6.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useRef} from 'react';
 import * as echarts from 'echarts';
 import {createEchartsOptions} from '../shared/create-echarts-options';
-import {px} from '../shared/px';
 import china from '../geo/china.json';
+import { px } from '../shared/px';
 
 export const Chart6 = () => {
-  const divRef = useRef<HTMLDivElement | null>(null);
+  const divRef = useRef<HTMLDivElement>(null);
   const colors = {'青海省': '#BB31F7', '甘肃省': '#15B8FD', '四川省': '#06E1EE'};
   useEffect(() => {
     var myChart = echarts.init(divRef.current as HTMLDivElement);
@@ -19,52 +19,44 @@ export const Chart6 = () => {
           type: 'map',
           map: 'CN', // 自定义扩展图表类型
           data: [
-            {name: '甘肃省', value: 1},
+            {
+              name: '甘肃省', value: 1,
+              itemStyle:{
+                areaColor: colors['甘肃省']
+              },
+              label:{
+                formatter: ''
+              }
+            },
+            {
+              name: '四川省', value: 10,
+              itemStyle:{
+                areaColor: colors['四川省']
+              },
+              label:{
+                formatter: ''
+              }
+            },
+            {
+              name: '青海省', value: 10, 
+              itemStyle:{
+                areaColor: colors['青海省']
+              },
+              label:{
+                formatter: ''
+              }
+            },
           ],
-          label: {show: false, color: 'white'},
-          emphasis: {
-            label: {color: 'white'},
-            areaColor: '#5470C6',
-          },
+          label: {show: false, color: 'white', fontSize:px(12)},
           itemStyle: {
             areaColor: '#010D3D',
-            color: colors['甘肃省'],
-            borderColor: '#01A7F7',
-          }
-        },
-        {
-          type: 'map',
-          map: 'CN', // 自定义扩展图表类型
-          data: [
-            {name: '四川省', value: 100},
-          ],
+            borderColor: '#01A7F7'
+          },
           emphasis: {
             label: {color: 'white'},
-            areaColor: '#5470C6',
+            itemStyle:{areaColor: '#5470C6'}
           },
-          itemStyle: {
-            areaColor: '#010D3D',
-            color: colors['四川省'],
-            borderColor: 'yellow',
-          }
-        },
-        {
-          type: 'map',
-          map: 'CN', // 自定义扩展图表类型
-          data: [
-            {name: '青海省', value: 100},
-          ],
-          emphasis: {
-            label: {color: 'white'},
-            areaColor: '#5470C6',
-          },
-          itemStyle: {
-            areaColor: '#010D3D',
-            color: colors['青海省'],
-            borderColor: '#01A7F7',
-          }
-        },
-
+        }
       ]
     }));
   }, []);
@@ -75,7 +67,7 @@ export const Chart6 = () => {
       <div className="wrapper">
         <div ref={divRef} className="chart"/>
         <div className="legend bordered">
-        <span className="icon" style={{background: colors['甘肃省']}}/>甘岭籍
+          <span className="icon" style={{background: colors['甘肃省']}}/>甘岭籍
           <span className="icon" style={{background: colors['四川省']}}/>边城籍
           <span className="icon" style={{background: colors['青海省']}}/>寒国籍
         </div>
