5efc6074aa4fd25025ea299597a74cce43530ae7
FrankLai <laihongjiang@foxmail.com>
2022-10-19 01:04:29 +0800

    完成 Chart9

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -27,7 +27,7 @@ export const Home = () => {
         <section className='bordered section3'>
           <Chart5/>
         </section>
-        <section className='bordered section4'>
+        <section className='section4'>
           <Chart6/>
           <div className='bordered statistics7'>
             <h2>犯罪人员年龄段分布</h2>
